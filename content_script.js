363d53cce133a5f6fbe4d0ae78c1aa5e0248d57b
Spencer West <spencer.west@kbb.com>
2017-09-18 15:55:08 -0700

    first commit

---
+++ content_script.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+  if (document.querySelector('#meet-me-control-form') !== null) {
+    var elem = document.querySelector('#meet-me-control-form');
+    var firstElem = elem.querySelector('#meet-me-button-yes');
+    firstElem.click();
+    console.log('clicked button!');
+  }
+}, 1000);
