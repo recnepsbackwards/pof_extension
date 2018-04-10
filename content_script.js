8971d1c558a6829f911283c54d7c3d2e3308364c
Spencer West <spencer.west@kbb.com>
2018-04-09 22:24:57 -0700

    updates selector

--- content_script.js
+++ content_script.js
@@ -1,7 +1,7 @@
 setInterval(function(){
   if (document.querySelector('#meet-me-control-form') !== null) {
     var elem = document.querySelector('#meet-me-control-form');
-    var firstElem = elem.querySelector('#meet-me-button-yes');
+    var firstElem = elem.querySelector('#meet-me-button-yes-var');
     firstElem.click();
     console.log('clicked button!');
   }
